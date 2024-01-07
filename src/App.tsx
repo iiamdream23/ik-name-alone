5be84d8bc7a6ed305ba55b603f1a50fe74cbdd14
Imala Ikechukwu <ike4everwilling@yahoo.com>
2024-01-07 18:39:40 +0100

    ik commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const App: React.FC = () => {
+  return (
+    <div className='h-80 w-screen bg-gray-100 flex items-center p-20'>
+      <div>
+        <div className="text-4xl text-blue-500 font-bold">
+          <button className='bg-white rounded-full py-5 px-10 hover:text-white hover:bg-blue-500'>
+            ik
+          </button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
